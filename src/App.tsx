9b75f5becfd2982ff6527382da787f797c8f02d6
viteshbava <82492234+viteshbava@users.noreply.github.com>
2023-09-26 15:31:58 +1300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import About from './pages/About';
+
+Route;
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <Container className='mb-4'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  );
 };
 
 export default App;
